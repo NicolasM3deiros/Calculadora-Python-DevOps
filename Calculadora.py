bcb8b8753b336bbff996014df341eeb5e8080df9
Nicolas Medeiros <nicolas.medeiros332@gmail.com>
2026-04-19 19:01:45 -0300

    feat: cria grade visual dos botoes

--- Calculadora.py
+++ Calculadora.py
@@ -15,7 +15,24 @@ class Calculadora(ctk.CTk):
 
         # display de resultados
         self.display = ctk.CTkEntry(self, width=280, height=50, font=("Arial", 30), justify="right")
-        self.display.grid(row=0, column=0, columnspan=4, padx=10, pady=10)
+        self.display.grid(row=0, column=0, columnspan=4, padx=10, pady=20)
+
+        self.criar_botoes()
+
+    def criar_botoes(self):
+        # mapa dos botoes
+        botoes = [
+            ['7', '8', '9', '/'],
+            ['4', '5', '6', '*'],
+            ['1', '2', '3', '-'],
+            ['C', '0', '=', '+']
+        ]
+
+        # renderiza a grade
+        for linha_idx, linha in enumerate(botoes):
+            for col_idx, texto in enumerate(linha):
+                btn = ctk.CTkButton(self, text=texto, width=65, height=50, font=("Arial", 20))
+                btn.grid(row=linha_idx+1, column=col_idx, padx=5, pady=5)
 
 if __name__ == "__main__":
     app = Calculadora()
