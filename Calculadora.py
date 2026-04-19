eb38030b0fd2a368c9a97c9acec3f913d93eef02
Nicolas Medeiros <nicolas.medeiros332@gmail.com>
2026-04-19 18:54:45 -0300

    feat: configuração inicial da janela com customtkinter

--- Calculadora.py
+++ Calculadora.py
@@ -1,1 +1,18 @@
-print("Teste de repositório")
+import customtkinter as ctk
+
+# conf tema padrao
+ctk.set_appearance_mode("dark")
+ctk.set_default_color_theme("blue")
+
+class Calculadora(ctk.CTk):
+    def __init__(self):
+        super().__init__()
+
+        # setup da janela
+        self.title("Calculadora")
+        self.geometry("300x400")
+        self.resizable(False, False)
+
+if __name__ == "__main__":
+    app = Calculadora()
+    app.mainloop()
